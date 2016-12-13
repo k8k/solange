18b2b5adee3e4ea47c4bf881517693f47ede291e
Kate Kuchin <kate.kuchin@gmail.com>
2016-12-13 11:59:01 -0800

    change const/let to var so non-es6 humans can run

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const notifier = require('node-notifier');
-const request = require('request')
+var notifier = require('node-notifier');
+var request = require('request')
 
-const EVENTBRITE_TOKEN = process.env.EVENTBRITE_TOKEN
-let firstCapacity = 50
+var EVENTBRITE_TOKEN = process.env.EVENTBRITE_TOKEN
+var firstCapacity = 50
 
 setInterval(function () {
 	request('https://www.eventbriteapi.com/v3/events/30224092073/?token=' + EVENTBRITE_TOKEN, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	    console.log(JSON.parse(body).capacity)
-	    let capacity = JSON.parse(body).capacity
+	    var capacity = JSON.parse(body).capacity
 	    if (capacity !== firstCapacity) {
 	    	notifier.notify({
 	    		'title': 'SOLANGE!!!!',
