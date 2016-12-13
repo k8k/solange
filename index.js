90d2c3293025233319bf9c92478ef88dcdb3c271
Kate Kuchin <kate.kuchin@gmail.com>
2016-12-13 12:38:42 -0800

    convert tabs > spaces add noise

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ var EVENTBRITE_TOKEN = process.env.EVENTBRITE_TOKEN
 var firstCapacity = 50
 
 setInterval(function () {
-	request('https://www.eventbriteapi.com/v3/events/30224092073/?token=' + EVENTBRITE_TOKEN, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-	    console.log(JSON.parse(body).capacity)
-	    var capacity = JSON.parse(body).capacity
-	    if (capacity !== firstCapacity) {
-	    	notifier.notify({
-	    		'title': 'SOLANGE!!!!',
-	    		'message': 'go buy your tickets'
-	    	})
-	    }
-	  }
-	})	
+  request('https://www.eventbriteapi.com/v3/events/30224092073/?token=' + EVENTBRITE_TOKEN, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(JSON.parse(body).capacity)
+      var capacity = JSON.parse(body).capacity
+      if (capacity !== firstCapacity) {
+        notifier.notify({
+          'title': 'SOLANGE!!!!',
+          'message': 'go buy your tickets',
+          'sound': 'Ping'
+        })
+      }
+    }
+  })
 }, 7000)
 
