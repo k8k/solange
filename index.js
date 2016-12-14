8ad3a04a6aebbbba95e4616a79f8d6d2fdaa7356
Kate Kuchin <kate.kuchin@gmail.com>
2016-12-13 19:45:22 -0800

    updated cap to current, removed some magic numeros

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ var notifier = require('node-notifier');
 var request = require('request')
 
 var EVENTBRITE_TOKEN = process.env.EVENTBRITE_TOKEN
-var firstCapacity = 50
+var GET_REQUEST_URL = 'https://www.eventbriteapi.com/v3/events/30224092073/?token=' + EVENTBRITE_TOKEN
+var INTERVAL = 10000 // in ms
+var firstCapacity = 108
 
+// Check and see if capacity has updated, if so send a notification repeatedly and annoyingly
 setInterval(function () {
-  request('https://www.eventbriteapi.com/v3/events/30224092073/?token=' + EVENTBRITE_TOKEN, function (error, response, body) {
+  request(GET_REQUEST_URL, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(JSON.parse(body).capacity)
+      console.log('current capacity', JSON.parse(body).capacity)
       var capacity = JSON.parse(body).capacity
       if (capacity !== firstCapacity) {
         notifier.notify({
           'title': 'SOLANGE!!!!',
-          'message': 'go buy your tickets',
+          'message': 'Go buy your tickets, current capacity: ' + capacity,
           'sound': 'Ping'
         })
       }
     }
   })
-}, 7000)
+}, INTERVAL)
 
